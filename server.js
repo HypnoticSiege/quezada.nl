f976df52f60d4aa703dc1514c79ef3be9dfb4cf7
HypnoticSiege <luis@hypnoticsiege.net>
2022-09-02 00:50:50 -0400

    resume page + pdf

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 var options = {
   dotfiles: 'ignore',
   etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg', 'pdf'],
   index: ['index.html'],
   maxAge: '1m',
   redirect: false
