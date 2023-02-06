2fe03a1e34261ecc243e579df414191bc8880aff
HypnoticSiege <65150090+HypnoticSiege@users.noreply.github.com>
2023-02-05 19:01:25 -0500

    add(google analytics)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
+    .use(VueAnalytics, {
+        id: 'G-139GHCSZ2W',
+    })
