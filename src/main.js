2182d9ccdf0e4c4f2b82b34bc8bb631768f19c03
HypnoticSiege <luis@hypnoticsiege.net>
2023-02-27 11:00:59 -0500

    rm(g-anal)

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueAnalytics from 'vue-analytics'
 
 createApp(App)
     .use(router)
     .mount('#app')
-    .use(VueAnalytics, {
-        id: 'G-139GHCSZ2W',
-    })
